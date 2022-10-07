180b045d4e524998446ca24a5f8021b11de1bd2b
caseynv <nwandukelechi@gmail.com>
2022-10-07 10:24:31 +0100

    update

--- components/text/styles/index.styles.ts
+++ components/text/styles/index.styles.ts
@@ -11,16 +11,13 @@ export const TextWrapper = styled.div<TextPropsType>`
   font-weight: ${({weight}) => weight ?weight : "400"};
   line-height: ${({lineHeight}) => lineHeight ? lineHeight : "32px"};
   align: ${({align}) => align ? align : ""};
-<<<<<<< HEAD
   text-align: ${({ textalign }) => (textalign ? textalign : "")};
   font-weight: ${({ fontweight }) => (fontweight ? fontweight : "")}; ;
-
-  `
-=======
-
+  
   @media (max-width: ${Screen.sm}) {
     font-size: ${({ sm_size }) => (sm_size ? sm_size: "")};
     text-align: ${({sm_align}) => sm_align ? sm_align : ""};
   }
-`
->>>>>>> 07e4ee528b187a8f5514efcf58dcfe214163c97d
+  `
+
+  
