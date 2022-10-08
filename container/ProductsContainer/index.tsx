e25b98461bbe5c2025938b1b90cfcab0dabbc752
Adeola <boluoguna@gmail.com>
2022-10-08 07:06:54 +0100

    projects page

--- container/ProductsContainer/index.tsx
+++ container/ProductsContainer/index.tsx
@@ -15,7 +15,7 @@ export default function FocusContainer() {
             <Text.Small>Our Products</Text.Small>
             <Text lineHeight="48px" as="h1">Our dreams have grown bigger with time.</Text>
           </Stack>
-          <Stack m="48px 0 0 0" direction="row" justifyContent='space-between' alignItems='center' sm_direction='column' flexWrap='wrap'>
+          <Stack m="48px 0 0 0" direction="row" justifyContent='space-between' alignItems='center' md_direction='column' flexWrap='wrap'>
             <Stack direction="column" width="350px" height="469px">
               <Image width="100%" height="100%" objectFit='cover' layout='responsive' src={PhoneMockup} />
             </Stack>
@@ -31,7 +31,7 @@ export default function FocusContainer() {
               </Stack>
             </Stack>
           </Stack>
-          <Stack m="48px 0 0 0" direction="row-reverse" justifyContent='space-between' alignItems='center' sm_direction='column' flexWrap='wrap'>
+          <Stack m="48px 0 0 0" direction="row-reverse" justifyContent='space-between' alignItems='center' md_direction='column' flexWrap='wrap'>
             <Stack direction="column" width="350px" height="350px">
               <Image width="100%" height="100%" objectFit='contain' layout='responsive' src={LaptopMockup} />
             </Stack>
--- container/ProjectsContainer/styles/index.styles.ts
+++ container/ProjectsContainer/styles/index.styles.ts
@@ -29,5 +29,6 @@ export const ListProjectsWrapper = styled.div`
 
   @media(max-width: ${Screen.md}) {
     grid-template-columns: repeat(1, minmax(0, 1fr));
+    
   }
 `
