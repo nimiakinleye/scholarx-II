dc7a5105dd206f5a45865c29683de00fa32a3980
Adeola <boluoguna@gmail.com>
2022-10-12 08:38:12 +0100

    adding the contact form endpoint

--- container/FormContainer/index.tsx
+++ container/FormContainer/index.tsx
@@ -1,30 +1,61 @@
-import React from "react";
-import { Container, Text, Stack, Button } from "../../components";
+import React,{useState} from "react";
+import axios from "axios";
+import { Stack, Button } from "../../components";
 import { FormWrapper } from "./styles/index.styles";
 import { Colors } from "../../theme/colors";
+//import * as yup from "yup";
+
+
 
 export default function Form() {
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [phone_number, setNumber] = useState('');
+
+  
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    const data = {
+      firstname,
+      lastname,
+      email,
+      message,
+      phone_number
+    };
+    console.log(data);
+    
+
+    axios
+    .post('https://api.scholarx.co/api/v1/contact/submit', data)
+    .then((response) => {
+      console.log(response.data)
+    });
+  };
   return (
 <FormWrapper>
 <Stack p={"50px 20px 0"}>
       <form>
         <Stack direction="row" md_direction="column" gap={"16px"}>
           <Stack>
-            <label htmlFor="first">First Name</label>
+            <label htmlFor="firstName">First Name</label>
             <input
               type="text"
               id="first"
               name="first name"
               placeholder="input text here"
+              onChange={e => setFirstName(e.target.value)}
             />
           </Stack>
           <Stack>
-            <label htmlFor="first">Last Name</label>
+            <label htmlFor="lastName">Last Name</label>
             <input
               type="text"
               id="last"
               name="last name"
               placeholder="input text here"
+              onChange={e => setLastName(e.target.value)}
             />
           </Stack>
         </Stack>
@@ -35,6 +66,7 @@ export default function Form() {
             id="phone"
             name="phone number"
             placeholder="input text here"
+            onChange={e => setNumber(e.target.value)}
           />
         </Stack>
         <Stack>
@@ -44,14 +76,15 @@ export default function Form() {
             id="email"
             name="email"
             placeholder="input text here"
+            onChange={e => setEmail(e.target.value)}
           />
         </Stack>
       </form>
       <label htmlFor="help">How can we help you?</label>
-      <textarea placeholder="input text here" cols={30} rows={10} />
+      <textarea placeholder="input text here" cols={30} rows={10}  onChange={e => setMessage(e.target.value)}/>
      
       <Stack direction="row" justifyContent="center" m={"30px 0 50px 0"}>
-              <Button width="auto" type="submit" color={Colors.yellow} text={Colors.neutral}>
+              <Button width="auto" type="submit" color={Colors.yellow} text={Colors.neutral} onClick={handleSubmit}>
                 Submit
               </Button>
             </Stack>
