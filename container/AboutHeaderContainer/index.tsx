39c5c6df7766029049f2d0852781832a67cc388d
Adeola <boluoguna@gmail.com>
2022-10-24 15:43:13 +0100

    about header

--- container/AboutHeaderContainer/index.tsx
+++ container/AboutHeaderContainer/index.tsx
@@ -3,30 +3,52 @@ import Image from "next/image";
 import { Container, Stack, Text } from "../../components";
 import { HeaderContainer } from "..";
 import { Colors } from "../../theme/colors";
-import about from '../../assets/images/about-1.png'
+import about from "../../assets/images/about-1.png";
 
 export default function AboutHeader() {
   return (
     <Container>
       <Stack>
         <Stack m={"50px 0 0 0"}>
-          <HeaderContainer color={Colors.primary}/>
+          <HeaderContainer color={Colors.primary} />
         </Stack>
-        <Stack direction="row" m={"80px 0 0 0"} justifyContent={"space-between"} md_direction="column" gap={"51px"}>
+        <Stack
+          direction="row"
+          m={"80px 0 0 0"}
+          justifyContent={"space-between"}
+          md_direction="column"
+          gap={"51px"}
+        >
           <Stack className={"headerText"} width={"70%"} md_width={"100%"}>
-            <Text as="h4" color={Colors.primary} weight={"700"} sm_size={"21px"} size={"48px"}>About Us</Text>
-            <Text as="p" weight={"400"} color={Colors.gray_600} size={"18px"} lineHeight={"36px"} p={"24px 0 0 0"} >
+            <Text
+              as="h4"
+              color={Colors.primary}
+              weight={"700"}
+              sm_size={"21px"}
+              size={"48px"}
+            >
+              About Us
+            </Text>
+            <Text
+              as="p"
+              weight={"400"}
+              color={Colors.gray_600}
+              size={"18px"}
+              lineHeight={"36px"}
+              p={"24px 0 0 0"}
+            >
               Founded in 2015, ScholarX is a social impact startup that focuses
-              on Educational Financing. We are committed to helping young
+              on Educational Financing.
+              We are committed to helping young
               Africans access quality education and skills through Scholarships,
               Crowdfunding (Village), and E-Learning. By linking young people
               with financial aid both domestically and overseas, we are closing
               the funding gap for education.
             </Text>
           </Stack>
-          
-          <Stack className={"HeaderImage"}  >
-                <Image src={about} alt="some of the ScholarX team members"/>
+
+          <Stack className={"HeaderImage"}>
+            <Image src={about} alt="some of the ScholarX team members" />
           </Stack>
         </Stack>
       </Stack>
--- pages/project/index.tsx
+++ pages/project/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Hero, FooterContainer,ProjectsCards} from '../../container'
+import { Hero, FooterContainer, ProjectsCards} from '../../container'
 //import { AboutHeader, Team, FoundingTeam, Contact, FooterContainer} from '../../container'
 
 
